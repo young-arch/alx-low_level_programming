78caeb4071a2ce65b70d893a732068b44113c2fb
young-arch <mawulegabriel@gmail.com>
2022-07-02 05:27:38 -0700

    Another day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
+ 
+main - Entry point
+ 
+Return: Always 0 (Success)
+ 
  * */
 
 
@@ -16,8 +14,10 @@ int main(void)
 
 {
 
-  puts("\"Programming is like building a multilingual puzzle");
 
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+	return (0);
 
 }
